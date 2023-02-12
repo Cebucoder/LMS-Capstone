673d50538314db4ca63a9df79702b27b95a4fbbf
Cebucoder <danielabellana187@gmail.com>
2023-02-12 12:37:05 +0800

    initial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// let option  = document.querySelector("#option");
+// let calendar  = document.querySelector("#calendar");
+// let people  = document.querySelector("#people");
+// let book  = document.querySelector("#book");
+// let bar  = document.querySelector("#bar");
+// let mail  = document.querySelector("#mail");
+
+
+// myFunction = function(event) {
+//   // reset all menu items
+//   document.querySelectorAll('li.active').forEach(function(item) {
+//   item.classList.remove('active');
+// })
+//   // mark as active selected menu item
+//   event.target.classList.add("active");
+// };
