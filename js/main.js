3f31c2ffab857ee6848b68b44255d9ec579046f6
Cebucoder <danielabellana187@gmail.com>
2023-02-27 11:24:34 +0800

    initial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 let dash  = document.querySelector("#dashboard");
 let schedule  = document.querySelector("#incoming-lesson-container");
-let groups  = document.querySelector("#Groups-container");
+// let groups  = document.querySelector("#Groups-container");
 let subs  = document.querySelector("#Subject-container");
 let grade  = document.querySelector("#Grades-container");
 let inbox  = document.querySelector("#Inbox-container");
@@ -44,7 +44,7 @@ myFunction = function(event) {
 function dashboard() {
 	dash.classList.remove("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
@@ -55,7 +55,7 @@ function dashboard() {
 function calendar() {
 	dash.classList.add("hide-dashboard");
 	schedule.classList.add("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
@@ -65,7 +65,7 @@ function calendar() {
 function people() {
 	dash.classList.add("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.add("show-Groups-container");
+	// groups.classList.add("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
@@ -75,7 +75,7 @@ function people() {
 function subject() {
 	dash.classList.add("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.add("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
@@ -85,7 +85,7 @@ function subject() {
 function grades() {
 	dash.classList.add("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.add("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
@@ -96,7 +96,7 @@ function grades() {
 function inboxs() {
 	dash.classList.add("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.add("show-Inbox-container");
@@ -108,7 +108,7 @@ function inboxs() {
 function showStudent(){
 	dash.classList.add("hide-dashboard");
 	schedule.classList.remove("show-incoming-lesson");
-	groups.classList.remove("show-Groups-container");
+	// groups.classList.remove("show-Groups-container");
 	subs.classList.remove("show-Subject-container");
 	grade.classList.remove("show-Grades-container");
 	inbox.classList.remove("show-Inbox-container");
